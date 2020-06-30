b209c8c674f0958243af8750989302095fe12a25
Abel Segui <Abelsegui@hotmail.com>
2020-06-30 02:52:35 +0000

    card_update

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropType from "prop-types";
 
-const BootstrapCard = props => {
+export const Card = props => {
 	// 1) replace the hard-coded image, description, link, etc. With their property variable.
 	return (
 		<div className="card m-5">
-			<img className="card-img-top" src={PropType.imageUrl} alt="Card image cap" />
+			<img
+				className="card-img-top"
+				src={PropType.imageUrl}
+				alt="Card image cap"
+			/>
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
 				<p className="card-text">{props.description}</p>
@@ -17,7 +21,7 @@ const BootstrapCard = props => {
 		</div>
 	);
 };
-BootstrapCard.propTypes = {
+Card.propTypes = {
 	title: PropType.string,
 	// 2) add here the new properties into the proptypes object
 	imageUrl: PropType.href,
@@ -27,12 +31,13 @@ BootstrapCard.propTypes = {
 };
 
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
-ReactDOM.render(
-	<BootstrapCard
-		title="Bob Dylan"
-		imageUrl="https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/"
-		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
-		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
-		buttonLabel="Go to wikipedia"
-	/>, document.querySelector("#app")
-);
+// ReactDOM.render(
+// 	<BootstrapCard
+// 		title="Bob Dylan"
+// 		imageUrl="https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/"
+// 		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+// 		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+// 		buttonLabel="Go to wikipedia"
+// 	/>,
+// 	document.querySelector("#app")
+// );
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -17,10 +17,27 @@ export function Home() {
 		<div className="text-center mt-5">
 			<Navbar />
 			<Jumbotron />
-			<Card />
-			<Card />
-			<Card />
-			<Card />
+			<Card
+				title="Bob Dylan"
+				imageUrl="https://ucarecdn.com/f8cf81eb-3bab-4bba-9431-668884eab174/-/resize/300x/"
+				description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+				buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+				buttonLabel="Go to wikipedia"
+			/>
+			<Card
+				title="Albert Einstein"
+				imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/220px-Albert_Einstein_Head.jpg"
+				description="Albert Einstein (/ˈaɪnstaɪn/ EYEN-styne;[4] German: [ˈalbɛʁt ˈʔaɪnʃtaɪn] (About this soundlisten); 14 March 1879 – 18 April 1955) was a German-born theoretical physicist[5] who developed the theory of relativity, one of the two pillars of modern physics (alongside quantum mechanics)."
+				buttonUrl="https://en.wikipedia.org/wiki/Albert_Einstein"
+				buttonLabel="Go to wikipedia"
+			/>
+			<Card
+				title="Isaac Newton"
+				imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Sir_Isaac_Newton_%281643-1727%29.jpg/220px-Sir_Isaac_Newton_%281643-1727%29.jpg"
+				description="Isaac Newton ( Woolsthorpe , Lincolnshire , December 25, 1642 Jul. / 4 as January  as  1643 greg. - Kensington , London , on March 20 . July / March 31, 1727 . Greg ) was a physicist , theologian , inventor , alchemist and English mathematician ."
+				buttonUrl="https://en.wikipedia.org/wiki/Isaac_Newton"
+				buttonLabel="Go to wikipedia"
+			/>
 			<Footer />
 		</div>
 	);
